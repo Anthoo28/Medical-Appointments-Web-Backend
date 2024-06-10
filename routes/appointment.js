51aad77af02b086949312e9f3788ad3210fed3fc
Anthoo28 <zarek1404@gmail.com>
2024-06-09 22:19:30 -0500

    added appointment by doctor

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,11 +1,12 @@
 const {Router} = require('express');    
-const { getAppointments,getAppointmentById,createAppointment,deleteAppointment } = require('../controllers/appointmentController');
+const { getAppointments,getAppointmentById,createAppointment,deleteAppointment,getAppointmentByIdDoctor } = require('../controllers/appointmentController');
 
 const router = Router();
 
 
 router.get('/', getAppointments);
 router.get('/:id', getAppointmentById);
+router.get('/doctor/:id', getAppointmentByIdDoctor);
 router.post('/', createAppointment);
 router.delete('/:id', deleteAppointment);
 
